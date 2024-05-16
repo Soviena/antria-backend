5e6d7fac14348cb9b4821f18617759e597101b54
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2024-05-16 17:41:11 +0700

    add image to edit karyawan

--- src/karyawan/karyawan.controller.ts
+++ src/karyawan/karyawan.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { KaryawanService } from './karyawan.service';
 import { Karyawan } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
 
 
 @Controller('karyawan')
@@ -29,10 +33,45 @@ export class KaryawanController {
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() data: Karyawan): Promise<Karyawan> {
+  @UseInterceptors(FileInterceptor('profile_picture',{
+    storage: diskStorage({
+      destination: "./MediaUpload/",
+      filename: (req, file, callback) => {
+        const uniqueSuffix = uuidv4();
+        const fileExtName = path.extname(file.originalname);
+        const newFileName = `${uniqueSuffix}${fileExtName}`;
+        callback(null, newFileName);
+      }
+    })
+  }))
+  async update(@Param('id') id: string, @Body() data: Karyawan, @UploadedFile() file: Express.Multer.File): Promise<Karyawan> {
+    let { profile_picture } = data;
+    if (file != null) {
+      // if (profile_picture !== "") {
+      //   const filePath = __dirname+'../../MediaUpload/'+profile_picture;
+      //   fs.unlink(filePath, (err) => {
+      //     if (err) {
+      //       console.error(err);
+      //       return {
+      //         status: 'error',
+      //         message: 'File not found or could not be deleted',
+      //       };
+      //     }
+      //     console.log(`File ${profile_picture} deleted`);
+      //     return {
+      //       status: 'success',
+      //       message: `File ${profile_picture} deleted`,
+      //     };
+      //   });
+      // }
+      profile_picture = file.filename;
+    }
     return this.karyawanService.updateKaryawan({
       where: { id: parseInt(id) },
-      data,
+      data: {
+        profile_picture,
+        ...data
+      },
     });
   }
 
