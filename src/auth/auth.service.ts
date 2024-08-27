f4574b741f9f3377e48b786869f4f434f047546b
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2024-08-27 14:25:51 +0700

    fix auth forgot password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -103,7 +103,7 @@ export class AuthService {
         if (!user) {
           throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
         }
-        if (user.otp === otp || parseInt(user.otp) == parseInt(otp)){
+        if (user.otp === otp ||(parseInt(user.otp) == parseInt(otp))){
           await this.pelangganService.updatePelanggan({
             where: { email: email },
             data: {
@@ -130,7 +130,7 @@ export class AuthService {
         if (!user) {
           throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
         }
-        if (user.otp === otp){
+        if (user.otp === otp ||(parseInt(user.otp) == parseInt(otp))){
           await this.karyawanService.updateKaryawan({
             where: { email: email },
             data: {
